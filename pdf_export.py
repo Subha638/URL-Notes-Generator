1a6c968f26f3128a069fba2a407e2ac5bae289b1
Subha638 <subhalaxmi1227@gmail.com>
2025-11-23 09:29:06 +0530

    Update pdf_export.py

--- pdf_export.py
+++ pdf_export.py
@@ -6,13 +6,12 @@ def create_pdf(notes, faq, mcq, filename):
     pdf.set_auto_page_break(auto=True, margin=15)
 
     pdf.set_font("Arial", "B", 16)
-    pdf.cell(0, 10, "AI-Generated Study Notes", ln=True)
+    pdf.cell(0, 10, "AI-Generated Notes", ln=True)
 
     pdf.set_font("Arial", "", 12)
-
     pdf.multi_cell(0, 8, notes)
-    pdf.ln(5)
 
+    pdf.ln(5)
     pdf.set_font("Arial", "B", 14)
     pdf.cell(0, 10, "FAQs", ln=True)
 
