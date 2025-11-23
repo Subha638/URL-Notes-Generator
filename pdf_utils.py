699cf9706463157bd708cd7bc33700439f83e390
Subha638 <subhalaxmi1227@gmail.com>
2025-11-23 09:44:18 +0530

    Update and rename pdf_export.py to pdf_utils.py

---
+++ pdf_utils.py
@@ -0,0 +1,17 @@
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+
+def create_pdf(text, filename):
+    c = canvas.Canvas(filename, pagesize=letter)
+    width, height = letter
+
+    y = height - 50
+
+    for line in text.split("\n"):
+        if y < 50:
+            c.showPage()
+            y = height - 50
+        c.drawString(40, y, line)
+        y -= 20
+
+    c.save()
